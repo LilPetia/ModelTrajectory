f313797d596cdd2f5c63ebc6674905658c3fda35
LilPetia <kuznetsov.nikolai@phystech.edu>
2021-05-09 01:44:54 +0700

    add anotheer model with coefficient

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 from model_traectory import trajectory
 
 
+def startFisrtModel():
+    data = []
+    with open("data.txt", "r") as input:
+        i = 0
+        try:
+            for line in input:
+                data.append(float(line))
+                i += 1
+                if i > 5: raise ValueError
+        except ValueError:
+            print("Bad input")
+    with open("final_coordinates.txt", "w") as output:
+        output.write(str(tuple(trajectory.make_coordinates1(*data))))
+    pass
+
+
+def startSecondModel():
+    coefs = []
+    data = []
+    with open("data2.txt", "r") as input:
+        try:
+            i = 0
+            for line in input:
+                if i <= 4:
+                    data.append(float(line))
+                else:
+                    coefs.append(float(line))
+                i += 1
+                if i > 9: raise ValueError
+        except ValueError:
+            print("Bad input")
+    with open("final_coordinates2.txt", "w") as output:
+        output.write(str(tuple(trajectory.make_coordinates2(*data, coefs))))
+
+    pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    data = []
-    with open("data.txt","r") as input:
-        for line in input:
-            data.append(float(line))
-    with open("final_coordinates.txt", "w") as output:
-        output.write(str(tuple(trajectory.make_coordinates(*data))))
-
-
+    startFisrtModel()
+    startSecondModel()
--- model_traectory.py
+++ model_traectory.py
@@ -1,13 +1,33 @@
-from math import cos, sin
+from math import cos, sin, pi
 
 
 class trajectory:
     def __init__(self):
         pass
 
+    # start direction(alpha = 0) is direction is vector from center to enemy`s
+    # goal. x0, y0 is coordinates from field`s center
     @staticmethod
-    def make_coordinates(self, x0: float, y0: float, alpha: float,
-                         dist: float) -> [int, int]:
+    def make_coordinates1(self, x0: float, y0: float, alpha: float,
+                          dist: float) -> [int, int]:
         x1 = x0 + dist * cos(alpha)
         y1 = y0 + dist * sin(alpha)
+        return [x1, y1]
+
+    @staticmethod
+    def make_coordinates2(self, x0: float, y0: float, alpha: float,
+                          dist: float, coefficients: [float, float, float,
+                                                      float]) -> [int, int]:
+        # List of coefficients for all directions. First direction is vector
+        # from center to enemу`s goal, the following follow  clockwise
+        if alpha < pi / 2 and alpha > pi / 2:
+            x1 = x0 + coefficients[0] * dist * cos(alpha)
+            pass
+        else:
+            x1 = x0 + coefficients[2] * dist * cos(alpha)
+            pass
+        if (alpha > 0 and alpha < pi):
+            y1 = y0 + coefficients[1] * dist * sin(alpha)
+        else:
+            y1 = y0 + coefficients[3] * dist * sin(alpha)
         return [x1, y1]
